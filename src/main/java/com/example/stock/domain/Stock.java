90e3c2ae7f923f56d67d8aac639bd531eb48d1ae
hong9Lee <hongtemp9mac@gmail.com>
2023-10-07 17:25:08 +0900

    재고 감소 로직 race condition

---
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,45 @@
+package com.example.stock.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+
+    private Long productId;
+
+    private Long quantity;
+
+    public Stock() {
+    }
+
+    public Stock(Long productId, Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void decrease(Long quantity) {
+        if(this.quantity - quantity < 0) {
+            throw new RuntimeException("재고는 0개 미만이 될 수 없습니다.");
+        }
+        this.quantity -= quantity;
+    }
+}
---
+++ src/main/java/com/example/stock/service/StockService.java
@@ -0,0 +1,28 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class StockService {
+
+    private StockRepository stockRepository;
+
+    public StockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+
+        // Stock 조회
+        // 재고를 감소시킨 뒤, 갱신된 값을 저장한다.
+        Stock stock = stockRepository.findById(id).orElseThrow();
+        stock.decrease(quantity);
+
+        stockRepository.save(stock);
+    }
+}
